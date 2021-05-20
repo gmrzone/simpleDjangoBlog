6b4d0b72e6a85b5e649f1af31ab84c7523db82e8
Afzal Saiyed <saiyedafzalgz@gmail.com>
2021-05-20 07:22:51 -0700

    Update settings.py

--- bookfirst/settings.py
+++ bookfirst/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'saiyedafzal0@gmail.com'
-EMAIL_HOST_PASSWORD = '27021992Afzal'
+EMAIL_HOST_USER = os.environ['user']
+EMAIL_HOST_PASSWORD = os.environ['password']
